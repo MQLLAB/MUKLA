8101f432f9f57dfdd131cadfbe4cbde23393ad10
Egor Pervuninski <egorp@eneset.com>
2017-06-13 21:53:00 +1000

    Added test to check GoDoc

--- main.go
+++ main.go
@@ -1,3 +1,13 @@
+// This is where the description of what MUKLA does will go.
+//
+// Lorem ipsum dolor sit amet, te est epicurei senserit. Officiis dissentias
+// usu ad. At legimus percipitur vim. Eu dico stet persius vim, vim id everti
+// appareat. Id est alterum accusamus, eam no sumo prima viderer.
+//
+// Bonorum repudiare eos at, per augue quaestio incorrupte an. Has cu odio
+// ludus, vis ut ancillae albucius. Cum eu equidem rationibus. Mel utinam
+// facilis epicurei an, in congue apeirian has. Nisl inermis habemus pro et.
+// Mea ne munere inciderint, cum ut sumo tibique, no mea illum choro.
 package main
 
 import (
